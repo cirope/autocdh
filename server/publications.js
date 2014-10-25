ec2769051c58df7f7a8c359ac7b7b7b598d238bd
Franco Catena <francocatena@gmail.com>
2014-10-25 13:40:07 -0300

    Now we can add works to customers

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,30 @@
 Meteor.publish('customers', function () {
-  return Customers.find({ userId: this.userId })
+  var sub = this
+  var worksHandles = []
+
+  var publishWorks = function (customerId) {
+    var worksCursor = Works.find({ userId: sub.userId, customerId: customerId })
+
+    worksHandles[customerId] = Mongo.Collection._publishCursor(worksCursor, sub, 'works')
+  }
+
+  var customersHandle = Customers.find({ userId: sub.userId }).observeChanges({
+    added: function (id, customer) {
+      publishWorks(id)
+      sub.added('customers', id, customer)
+    },
+
+    changed: function (id, fields) {
+      sub.changed('customers', id, fields)
+    },
+
+    removed: function (id) {
+      worksHandles[id] && worksHandles[id].stop()
+      sub.removed('customers', id)
+    }
+  })
+
+  sub.ready()
+
+  sub.onStop(function () { customersHandle.stop() })
 })
