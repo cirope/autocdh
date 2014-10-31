86cb1b3782e68173587745137644928c3e1e4965
Franco Catena <francocatena@gmail.com>
2014-10-31 11:40:20 -0300

    Add concrete to sample show

--- client/views/samples/sample.js
+++ client/views/samples/sample.js
@@ -19,5 +19,9 @@ Template.sample.helpers({
 
   sampleReceipt: function () {
     return Receipts.findOne({ sampleId: this._id })
+  },
+
+  sampleConcrete: function () {
+    return Concretes.findOne({ sampleId: this._id })
   }
 })
