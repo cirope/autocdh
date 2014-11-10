1bd59eaa9f7c113adad9d70f0b0a2f93ae47d4a5
Franco Catena <francocatena@gmail.com>
2014-11-10 13:37:28 -0300

    Better dosage handling in concretes

--- client/views/concretes/new.js
+++ client/views/concretes/new.js
@@ -1,4 +1,51 @@
+var dosages   = new ReactiveVar([])
+var formulaId = new ReactiveVar
+
+Tracker.autorun(function () {
+  var strengthId   = AutoForm.getFieldValue('newConcreteForm', 'strengthId')
+  var downloadId   = AutoForm.getFieldValue('newConcreteForm', 'downloadId')
+  var aggregateId  = AutoForm.getFieldValue('newConcreteForm', 'aggregateId')
+  var settlementId = AutoForm.getFieldValue('newConcreteForm', 'settlementId')
+  var _formula     = Formulas.findOne({
+    strengthId:   strengthId,
+    downloadId:   downloadId,
+    aggregateId:  aggregateId,
+    settlementId: settlementId
+  })
+
+  formulaId.set(_formula && _formula._id)
+})
+
+Tracker.autorun(function () {
+  var _dosages   = []
+
+  if (formulaId.get()) {
+    var formula    = Formulas.findOne(formulaId.get())
+    var aggregates = _.union(formula.sands, formula.gravels)
+
+    _dosages.push({ name: TAPi18n.__('formula_concrete'), amount: formula.concrete })
+    _dosages.push({ name: TAPi18n.__('formula_water'), amount: formula.water })
+
+    aggregates.forEach(function (aggregate) {
+      _dosages.push({ name: aggregate.name, amount: aggregate.amount })
+    })
+  } else {
+    _dosages.push({ name: TAPi18n.__('formula_concrete') })
+    _dosages.push({ name: TAPi18n.__('formula_water') })
+    _dosages.push({ name: TAPi18n.__('formula_sand') })
+    _dosages.push({ name: TAPi18n.__('formula_gravel') })
+  }
+
+  dosages.set(_dosages)
+})
+
 Template.concreteNew.helpers({
+  doc: function () {
+    return {
+      dosages: dosages.get()
+    }
+  },
+
   strengthOptions: function () {
     return Strengths.find().map(function (strength) {
       return { value: strength._id, label: strength.name }
