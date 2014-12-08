ab734a83e544dd6653d5db70147eb94c38e8fedd
Franco Catena <francocatena@gmail.com>
2014-12-07 21:14:58 -0300

    Samples form now dried up...

--- client/views/samples/_new.js
+++ client/views/samples/_new.js
@@ -1,29 +1,3 @@
-Template._sampleNew.helpers({
-  namePlaceholder: function () {
-    return TAPi18n.__('sample_name_placeholder')
-  },
-
-  moldingOptions: function () {
-    return [
-      { value: 'plant',  label: TAPi18n.__('sample_molding_plant') },
-      { value: 'work',   label: TAPi18n.__('sample_molding_work') },
-      { value: 'remote', label: TAPi18n.__('sample_molding_remote') }
-    ]
-  },
-
-  plantOptions: function () {
-    return Plants.find().map(function (plant) {
-      return { value: plant._id, label: plant.name }
-    })
-  },
-
-  responsibleOptions: function () {
-    return Responsible.find().map(function (responsible) {
-      return { value: responsible._id, label: responsible.name }
-    })
-  }
-})
-
 AutoForm.addHooks('newSampleForm', {
   before: {
     createSample: function (doc, template) {
