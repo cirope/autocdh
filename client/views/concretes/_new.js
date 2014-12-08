6b2364a109ee229a203ed94f9a3e6c948dcabd56
Franco Catena <francocatena@gmail.com>
2014-12-08 18:05:05 -0300

    Concrete form improvements...

--- client/views/concretes/_new.js
+++ client/views/concretes/_new.js
@@ -1,20 +1,5 @@
-var dosages   = new ReactiveVar([])
+var doc       = new ReactiveVar({})
 var formulaId = new ReactiveVar
-
-Tracker.autorun(function () {
-  var strengthId   = AutoForm.getFieldValue('newConcreteForm', 'strengthId')
-  var downloadId   = AutoForm.getFieldValue('newConcreteForm', 'downloadId')
-  var aggregateId  = AutoForm.getFieldValue('newConcreteForm', 'aggregateId')
-  var settlementId = AutoForm.getFieldValue('newConcreteForm', 'settlementId')
-  var _formula     = Formulas.findOne({
-    strengthId:   strengthId,
-    downloadId:   downloadId,
-    aggregateId:  aggregateId,
-    settlementId: settlementId
-  })
-
-  formulaId.set(_formula && _formula._id)
-})
 
 Tracker.autorun(function () {
   var _dosages   = []
@@ -24,7 +9,7 @@ Tracker.autorun(function () {
     var aggregates = _.union(formula.sands, formula.gravels)
 
     _dosages.push({ name: TAPi18n.__('formula_concrete'), amount: formula.concrete, absorption: 0 })
-    _dosages.push({ name: TAPi18n.__('formula_water'), amount: formula.water, absorption: 0 })
+    _dosages.push({ name: TAPi18n.__('formula_water'),    amount: formula.water,    absorption: 0 })
 
     aggregates.forEach(function (aggregate) {
       _dosages.push({
@@ -35,61 +20,42 @@ Tracker.autorun(function () {
     })
   } else {
     _dosages.push({ name: TAPi18n.__('formula_concrete'), absorption: 0 })
-    _dosages.push({ name: TAPi18n.__('formula_water'), absorption: 0 })
+    _dosages.push({ name: TAPi18n.__('formula_water'),    absorption: 0 })
     _dosages.push({ name: TAPi18n.__('formula_sand') })
     _dosages.push({ name: TAPi18n.__('formula_gravel') })
   }
 
-  dosages.set(_dosages)
+  doc.set({ dosages: _dosages })
 })
 
 Template._concreteNew.helpers({
   doc: function () {
-    return {
-      dosages: dosages.get()
-    }
-  },
+    return doc.get()
+  }
+})
 
-  strengthOptions: function () {
-    return Strengths.find().map(function (strength) {
-      return { value: strength._id, label: strength.name }
+Template._concreteNew.events({
+  'change [data-formula-component]': function () {
+    var strengthId   = $('[name="strengthId"]').val()
+    var downloadId   = $('[name="downloadId"]').val()
+    var aggregateId  = $('[name="aggregateId"]').val()
+    var settlementId = $('[name="settlementId"]').val()
+    var _formula     = Formulas.findOne({
+      strengthId:   strengthId,
+      downloadId:   downloadId,
+      aggregateId:  aggregateId,
+      settlementId: settlementId
     })
-  },
 
-  downloadOptions: function () {
-    return Downloads.find().map(function (download) {
-      return { value: download._id, label: download.name }
-    })
-  },
-
-  aggregateOptions: function () {
-    return Aggregates.find().map(function (aggregate) {
-      return { value: aggregate._id, label: aggregate.name }
-    })
-  },
-
-  settlementOptions: function () {
-    return Settlements.find().map(function (settlement) {
-      return { value: settlement._id, label: settlement.name }
-    })
-  },
-
-  typeOptions: function () {
-    return Types.find().map(function (type) {
-      return { value: type._id, label: type.name }
-    })
-  },
-
-  additiveOptions: function () {
-    return Additives.find().map(function (additive) {
-      return { value: additive._id, label: additive.name }
-    })
+    formulaId.set(_formula && _formula._id)
   }
 })
 
 AutoForm.addHooks('newConcreteForm', {
   before: {
     createConcrete: function (doc, template) {
+      setTimeout(function () { formulaId.set(undefined) })
+
       return _.extend(doc, { _id: Random.id() })
     }
   }
