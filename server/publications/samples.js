01a87b9fe1cd709677b6333909a10c5211da1600
Franco Catena <francocatena@gmail.com>
2014-12-09 02:46:37 -0300

    Samples list paginated...

--- server/publications/samples.js
+++ server/publications/samples.js
@@ -1,3 +1,9 @@
 Meteor.publish('samples', function () {
   return Samples.find({ userId: this.userId })
 })
+
+Meteor.publish('samplesLimited', function (options) {
+  check(options, { sort:  Object, limit: Number })
+
+  return Samples.find({ userId: this.userId }, options)
+})
