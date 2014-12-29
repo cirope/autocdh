5befeeaf35068587255f8d0601fcbd92e7674a68
Franco Catena <francocatena@gmail.com>
2014-12-28 21:27:25 -0300

    Better column handling in slim template...

--- client/views/templates/slim.js
+++ client/views/templates/slim.js
@@ -1,17 +1,21 @@
-var cols = 4
+var cols  = {}
+var route = function () {
+  return Router.current() && Router.current().route.getName()
+}
 
 Template.afArrayField_slim.helpers({
   cols: function () {
-    var attsCols = this.atts.cols
+    var attsCols = this.atts.cols || 3
+    var key      = route() + '.' + this.atts.name
 
-    if (attsCols) cols = 12 / +attsCols
-
-    return cols
+    return (cols[key] = cols[key] || 12 / +attsCols)
   }
 })
 
 Template.afFormGroup_slim.helpers({
   cols: function () {
-    return cols
+    var name = this.atts.name.substring(0, this.atts.name.indexOf('.'))
+
+    return cols[route() + '.' + name]
   }
 })
