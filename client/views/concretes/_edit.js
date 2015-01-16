62b9b36d808c32fb7058493189079cb48b601372
Franco Catena <francocatena@gmail.com>
2015-01-16 12:40:16 -0300

    Sort collections in concrete by creation date...

--- client/views/concretes/_edit.js
+++ client/views/concretes/_edit.js
@@ -1,24 +1,24 @@
 Template._concreteEdit.helpers({
   strengthOptions: function () {
-    return Strengths.find({}, { sort: { name: 1 } }).map(function (strength) {
+    return Strengths.find({}, { sort: { createdAt: 1 } }).map(function (strength) {
       return { value: strength._id, label: strength.name }
     })
   },
 
   downloadOptions: function () {
-    return Downloads.find({}, { sort: { name: 1 } }).map(function (download) {
+    return Downloads.find({}, { sort: { createdAt: 1 } }).map(function (download) {
       return { value: download._id, label: download.name }
     })
   },
 
   aggregateOptions: function () {
-    return Aggregates.find({}, { sort: { name: 1 } }).map(function (aggregate) {
+    return Aggregates.find({}, { sort: { createdAt: 1 } }).map(function (aggregate) {
       return { value: aggregate._id, label: aggregate.name }
     })
   },
 
   settlementOptions: function () {
-    return Settlements.find({}, { sort: { name: 1 } }).map(function (settlement) {
+    return Settlements.find({}, { sort: { createdAt: 1 } }).map(function (settlement) {
       return { value: settlement._id, label: settlement.name }
     })
   }
