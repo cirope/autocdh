6e9649be2466af273144034794c49f0cbfb52164
Franco Catena <francocatena@gmail.com>
2015-02-11 16:30:56 -0300

    Add additives edition and list as "parameters"

--- client/views/additives/new.js
+++ client/views/additives/new.js
@@ -3,8 +3,12 @@ Template.additiveNew.helpers({
     return TAPi18n.__('additive_name_placeholder')
   },
 
-  sampleId: function () {
-    return Router.current() && Router.current().params.sample_id
+  backPath: function () {
+    var params = Router.current() && Router.current().params
+
+    return params && params.sample_id ?
+      Router.path('concreteNew', { sample_id: params.sample_id }) :
+      Router.path('parameters')
   }
 })
 
