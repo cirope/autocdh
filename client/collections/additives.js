cf5e8d1a6e31d62187ab38b79c957471bfbecc1f
Franco Catena <francocatena@gmail.com>
2015-02-18 11:41:03 -0300

    Add units in loadAdditives...

--- client/collections/additives.js
+++ client/collections/additives.js
@@ -1,10 +1,10 @@
 loadAdditives = function () {
   if (Meteor.user() && ! Additives.find().count()) {
-    Additives.insert({ name: 'Retardador' })
-    Additives.insert({ name: 'Acelerante' })
-    Additives.insert({ name: 'Hiperfluidificante' })
-    Additives.insert({ name: 'Incorporador de aire' })
-    Additives.insert({ name: 'Hidrófugo' })
-    Additives.insert({ name: 'Fibras' })
+    Additives.insert({ name: 'Retardador',           unit: 'l/m³' })
+    Additives.insert({ name: 'Acelerante',           unit: 'l/m³' })
+    Additives.insert({ name: 'Hiperfluidificante',   unit: 'l/m³' })
+    Additives.insert({ name: 'Incorporador de aire', unit: 'l/m³' })
+    Additives.insert({ name: 'Hidrófugo',            unit: 'l/m³' })
+    Additives.insert({ name: 'Fibras',               unit: 'l/m³' })
   }
 }
