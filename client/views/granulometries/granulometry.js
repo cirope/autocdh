c3422f5e5563dc1a9e0c236ea3120230ef32b638
Franco Catena <francocatena@gmail.com>
2015-03-01 18:42:39 -0300

    Complete granulometry columns in _show_ view...

--- client/views/granulometries/granulometry.js
+++ client/views/granulometries/granulometry.js
@@ -1,3 +1,22 @@
+var retainedAccumulated = 0
+var retainedTotal       = 0
+var thinPercentage      = 0
+var correction          = 0
+var sampleWeight        = 0
+var passedPercentage    = 0
+var passedAccumulated   = 0
+
+var initVars            = function () {
+  retainedAccumulated = 0
+  passedPercentage    = 0
+  passedAccumulated   = 0
+  sampleWeight        = _.reduce(this.test, function (memo, t) {
+    return memo + t.grossWeight - t.netWeight
+  }, 0)
+  correction          = Math.round(sampleWeight * thinPercentage / 100)
+  retainedTotal       = sampleWeight + correction
+}
+
 Template.granulometry.helpers({
   type: function () {
     return TAPi18n.__('granulometry_type_' + this.type)
@@ -30,6 +49,55 @@ Template.granulometry.helpers({
     var netBefore = this.thin.massBeforeWash - this.thin.massOfContainer
     var netAfter  = this.thin.massAfterWash  - this.thin.massOfContainer
 
-    return netAfter > 0 ? ((netBefore - netAfter) / netAfter * 100).toFixed(1) : 0
+    thinPercentage = netAfter > 0 ? (netBefore - netAfter) / netAfter * 100 : 0
+
+    return thinPercentage.toFixed(1)
+  },
+
+  test: function () {
+    initVars.apply(this)
+
+    return this.test
+  },
+
+  retained: function () {
+    return this.grossWeight - this.netWeight
+  },
+
+  retainedAccumulated: function () {
+    retainedAccumulated += this.grossWeight - this.netWeight
+
+    return retainedAccumulated
+  },
+
+  correction: function () {
+    return correction
+  },
+
+  retainedTotal: function () {
+    return retainedTotal
+  },
+
+  sampleWeight: function () {
+    return sampleWeight
+  },
+
+  passed: function () {
+    return retainedTotal - retainedAccumulated
+  },
+
+  passedPercentage: function () {
+    passedPercentage   = Math.round((retainedTotal - retainedAccumulated) / retainedTotal * 100)
+    passedAccumulated += passedPercentage
+
+    return passedPercentage
+  },
+
+  retainedAccumulatedPercentage: function () {
+    return 100 - passedPercentage
+  },
+
+  fineness: function () {
+    return (passedAccumulated / 100).toFixed(2)
   }
 })
