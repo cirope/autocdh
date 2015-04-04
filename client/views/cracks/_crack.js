f8af0ddb18c107b25e891e5099fb27cda75f7fa4
Franco Catena <francocatena@gmail.com>
2015-04-04 19:12:38 -0300

    Move cracks remove redirection to method callback

--- client/views/cracks/_crack.js
+++ client/views/cracks/_crack.js
@@ -17,6 +17,8 @@ Template._crack.helpers({
 Template._crack.events({
   'click [data-delete]': function (event, template) {
     if (confirm(TAPi18n.__('confirm_delete')))
-      Meteor.call('removeCrack', template.data._id)
+      Meteor.call('removeCrack', template.data._id, function (error) {
+        if (! error) Router.go('cracks')
+      })
   }
 })
