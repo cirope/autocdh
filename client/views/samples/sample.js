3af5e104ee61c630d650258eec65f9dd33147a52
Franco Catena <francocatena@gmail.com>
2015-04-04 19:09:33 -0300

    Use method callback for sample redirect...

--- client/views/samples/sample.js
+++ client/views/samples/sample.js
@@ -122,7 +122,9 @@ var assay = function (assay) {
 Template.sample.events({
   'click [data-delete]': function (event, template) {
     if (confirm(TAPi18n.__('confirm_delete')))
-      Meteor.call('removeSample', template.data.sample._id)
+      Meteor.call('removeSample', template.data.sample._id, function (error) {
+        if (! error) Router.go('samples')
+      })
   },
 
   'click [data-download-pdf]': function (event, template) {
