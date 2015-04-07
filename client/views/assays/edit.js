f9072db65b81b3146397f13fa9f152b1831ec8cd
Franco Catena <francocatena@gmail.com>
2015-04-07 07:00:48 -0300

    Now you can update assay tubes...
    ... And cracks will be regenerated accordingly...

--- client/views/assays/edit.js
+++ client/views/assays/edit.js
@@ -1,5 +1,23 @@
+var tubeWarning = new ReactiveVar
+
+Template.assayEdit.onDestroyed(function () {
+  tubeWarning.set()
+})
+
 Template.assayEdit.helpers({
   sample: function () {
     return Samples.findOne(this.sampleId)
+  },
+
+  tubeWarning: function () {
+    return tubeWarning.get()
+  }
+})
+
+Template.assayEdit.events({
+  'change [name="tubes"]': function (event, template) {
+    var warning = template.data.tubes !== +$(event.currentTarget).val()
+
+    tubeWarning.set(warning)
   }
 })
