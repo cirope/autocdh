ae5b65960a40e67d9ef117bf54f553575b19cc3a
Franco Catena <francocatena@gmail.com>
2015-04-09 23:56:03 -0300

    Move test calculus to granulometry controller...

--- client/views/granulometries/granulometry.js
+++ client/views/granulometries/granulometry.js
@@ -1,30 +1,3 @@
-var type                = null
-var retainedTotal       = 0
-var thinPercentage      = 0
-var correction          = 0
-var sampleWeight        = 0
-var passedPercentage    = 0
-var passedAccumulated   = 0
-var retainedAccumulated = 0
-var retained            = []
-var chartData           = []
-
-var initVars = function () {
-  var self            = this
-
-  type                = self.type
-  chartData           = []
-  retained            = []
-  retainedAccumulated = 0
-  passedPercentage    = 0
-  passedAccumulated   = 0
-  sampleWeight        = _.reduce(self.test, function (memo, t) {
-    return memo + (type === 'sand' ? t.grossWeight - t.netWeight : t.netWeight)
-  }, 0)
-  correction          = Math.round(sampleWeight * thinPercentage / 100)
-  retainedTotal       = sampleWeight + correction
-}
-
 var graphLabels = function () {
   var seedLabels = _.pluck(this.data.test, 'sieve').slice(0, -1).reverse()
 
@@ -38,7 +11,7 @@ var graphLabels = function () {
 }
 
 var graphData = function () {
-  var seedData = chartData.slice(0, -1).reverse()
+  var seedData = this.data.chartData.slice(0, -1).reverse()
 
   if (type === 'gravel') {
     seedData.splice(1, 0, (seedData[0] + seedData[1]) / 2)
@@ -48,6 +21,12 @@ var graphData = function () {
 
   return seedData
 }
+
+var type = 'sand'
+
+Template.granulometry.onCreated(function () {
+  type = this.data.type
+})
 
 Template.granulometry.onRendered(function () {
   var data = {
@@ -74,7 +53,7 @@ Template.granulometry.helpers({
   },
 
   sand: function () {
-    return (this.type || type) === 'sand'
+    return type === 'sand'
   },
 
   responsible: function () {
@@ -93,85 +72,8 @@ Template.granulometry.helpers({
     return TAPi18n.__(this.washed ? 'yes' : 'no')
   },
 
-  humidityPercentage: function () {
-    var netWet = this.humidity && this.humidity.massOfWetAggregate - this.humidity.massOfContainer
-    var netDry = this.humidity && this.humidity.massOfDryAggregate - this.humidity.massOfContainer
-
-    return netDry > 0 ? ((netWet - netDry) / netDry * 100).toFixed(1) : 0
-  },
-
-  thinPercentage: function () {
-    var netBefore = this.thin && this.thin.massBeforeWash - this.thin.massOfContainer
-    var netAfter  = this.thin && this.thin.massAfterWash  - this.thin.massOfContainer
-
-    thinPercentage = netAfter > 0 ? (netBefore - netAfter) / netBefore * 100 : 0
-
-    return thinPercentage.toFixed(1)
-  },
-
-  test: function () {
-    initVars.apply(this)
-
-    return this.test
-  },
-
-  retained: function () {
-    return type === 'sand' ? this.grossWeight - this.netWeight : this.netWeight
-  },
-
-  retainedAccumulated: function () {
-    var retained = type === 'sand' ? this.grossWeight - this.netWeight : this.netWeight
-
-    retainedAccumulated += retained
-
-    return retainedAccumulated
-  },
-
-  correction: function () {
-    return correction
-  },
-
-  retainedTotal: function () {
-    return retainedTotal
-  },
-
-  sampleWeight: function () {
-    return sampleWeight
-  },
-
-  passed: function () {
-    return retainedTotal - retainedAccumulated
-  },
-
-  passedPercentage: function () {
-    passedPercentage   = Math.round((retainedTotal - retainedAccumulated) / retainedTotal * 100)
-    passedAccumulated += passedPercentage
-
-    chartData.push(passedPercentage)
-
-    return passedPercentage
-  },
-
-  retainedAccumulatedPercentage: function () {
-    retained.push(100 - passedPercentage)
-
-    return 100 - passedPercentage
-  },
-
   fineness: function () {
-    var fineness = 0
-
-    if (type === 'sand') {
-      fineness = _.reduce(retained.slice(0, -1), function (memo, n) { return memo + n }, 0) / 100
-    } else {
-      var indexes = [0, 3, 5, 7, 8, 9]
-
-      fineness = _.reduce(indexes, function (memo, i) {
-        return memo + retained[i]
-      }, 400 - 3 * thinPercentage) / 100
-    }
-
-    return fineness.toFixed(2)
+    return this.fineness.toFixed(2)
   }
 })
 
