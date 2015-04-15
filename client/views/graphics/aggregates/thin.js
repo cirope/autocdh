141d3c13548c7d7f5b0b9dec4effd652d3acc630
Franco Catena <francocatena@gmail.com>
2015-04-14 23:14:41 -0300

    Add granulometries thin graphic...

---
+++ client/views/graphics/aggregates/thin.js
@@ -0,0 +1,35 @@
+var updateChart = function (data) {
+  setTimeout(function () {
+    if ($('[data-chart]').length) {
+      var options = {
+        low:   0,
+        showLine: true,
+        axisX: {
+          labelInterpolationFnc: function (value, index) {
+            var module = Math.round(data.labels.length / 24)
+
+            return index % module === 0 ? value : null
+          }
+        }
+      }
+
+      var chart = new Chartist.Line('[data-chart]', data, options)
+
+      chart.on('draw', function (data) {
+        if (data.type === 'point' && data.value <= 0) data.element.attr({ class: 'hidden' })
+      })
+    }
+  })
+}
+
+Template.graphicAggregateThin.onRendered(function () {
+  updateChart(_.pick(this.data, 'labels', 'series'))
+})
+
+Template.graphicAggregateThin.helpers({
+  granulometriesCount: function () {
+    updateChart(_.pick(this, 'labels', 'series'))
+
+    return this.granulometries.count()
+  }
+})
