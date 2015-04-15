a799cf951f698c839473c01950e1f2f0efe5fb4e
Franco Catena <francocatena@gmail.com>
2015-04-14 23:34:31 -0300

    Show strength on cracks view...

--- client/views/cracks/_crack.js
+++ client/views/cracks/_crack.js
@@ -11,6 +11,13 @@ Template._crack.helpers({
     return this.otherAssay ?
       TAPi18n.__('assay_other_assay_' + this.otherAssay) :
       TAPi18n.__('no')
+  },
+
+  strength: function () {
+    var concrete = Concretes.findOne({ sampleId: this.sampleId })
+    var strength = concrete && Strengths.findOne(concrete.strengthId)
+
+    return strength && strength.name
   }
 })
 
