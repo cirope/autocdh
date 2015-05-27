4e758da1bfb2c857da9e97f07f1e1513040d764e
Franco Catena <francocatena@gmail.com>
2015-05-27 15:48:42 -0300

    Resistance at 7 days graphic...

---
+++ client/views/graphics/hardened_concrete/resistance_7_days.js
@@ -0,0 +1,36 @@
+var updateChart = function (data) {
+  setTimeout(function () {
+    if ($('[data-chart]').length) {
+      var options = {
+        low:      0,
+        showLine: true,
+        axisX: {
+          labelInterpolationFnc: function (value, index) {
+            var module = Math.round(data.labels.length / 24)
+
+            return index % module === 0 ? value : null
+          }
+        }
+      }
+
+      var chart = new Chartist.Line('[data-chart]', data, options)
+
+      chart.on('draw', function (data) {
+        if (data.type === 'point' && data.value <= 0)
+          data.element.attr({ class: 'hidden' })
+      })
+    }
+  }, 200)
+}
+
+Template.graphicHardenedConcrete7DaysResistance.onRendered(function () {
+  updateChart(_.pick(this.data, 'labels', 'series'))
+})
+
+Template.graphicHardenedConcrete7DaysResistance.helpers({
+  sampleCount: function () {
+    updateChart(_.pick(this, 'labels', 'series'))
+
+    return this.samples.count()
+  }
+})
