ae9a95432a806864ec0943b4c40795ec667ec4a3
Franco Catena <francocatena@gmail.com>
2015-07-01 14:52:50 -0300

    Add search to cracked cracks...

--- client/templates/cracks/cracked.js
+++ client/templates/cracks/cracked.js
@@ -6,3 +6,40 @@ Template.cracksCracked.helpers({
     return strength && strength.name
   }
 })
+
+Template.cracksCracked.events({
+  'click [data-action="search"]': function (event, template) {
+    var moldingIn = template.$('#molding-in').val()
+    var crackedIn = template.$('#cracked-in').val()
+
+    var search = {
+      designation: template.$('#designation').val(),
+      moldingIn:   moldingIn && moment(moldingIn, 'L').format('YYYY-MM-DD'),
+      crackedIn:   crackedIn && moment(crackedIn, 'L').format('YYYY-MM-DD'),
+      strength:    template.$('#strength').val(),
+      stress:      template.$('#stress').val()
+    }
+
+    Router.go('cracksCracked', {}, { query: search })
+  },
+
+  'click [data-search-clean]': function (event, template) {
+    template.$('#search-filter').collapse('hide')
+  },
+
+  'shown.bs.collapse': function (event, template) {
+    template.$('input:first').focus()
+
+    template.$('#molding-in').datetimepicker({ format: 'L' })
+    template.$('#cracked-in').datetimepicker({ format: 'L' })
+
+    template.$('[data-search-clean]').removeClass('hidden')
+    template.$('[data-search]').addClass('hidden')
+  },
+
+  'hidden.bs.collapse': function (event, template) {
+    template.$('[data-search-clean]').addClass('hidden')
+    template.$('[data-search]').removeClass('hidden')
+    template.$('input').val('')
+  }
+})
