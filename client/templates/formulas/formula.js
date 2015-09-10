abe352d82e3b16efff8b148b6bcf1533278992b9
Franco Catena <francocatena@gmail.com>
2015-09-10 00:31:28 -0300

    Now you can delete formulas

--- client/templates/formulas/formula.js
+++ client/templates/formulas/formula.js
@@ -30,18 +30,27 @@ Template.formula.helpers({
   },
 
   strength: function () {
-    return Strengths.findOne(this.strengthId).name
+    return this.strengthId && Strengths.findOne(this.strengthId).name
   },
 
   settlement: function () {
-    return Settlements.findOne(this.settlementId).name
+    return this.settlementId && Settlements.findOne(this.settlementId).name
   },
 
   aggregate: function () {
-    return Aggregates.findOne(this.aggregateId).name
+    return this.aggregateId && Aggregates.findOne(this.aggregateId).name
   },
 
   download: function () {
-    return TAPi18n.__('download_' + this.download)
+    return this.download && TAPi18n.__('download_' + this.download)
+  }
+})
+
+Template.formula.events({
+  'click [data-delete]': function (event, template) {
+    if (confirm(TAPi18n.__('confirm_delete')))
+      Meteor.call('removeFormula', template.data._id, function (error) {
+        if (! error) Router.go('formulas')
+      })
   }
 })
