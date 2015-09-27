df9ae0f33312d1ac2edf398fe75d5dc43e89fd1d
Franco Catena <francocatena@gmail.com>
2015-09-27 15:30:25 -0300

    Add Cusum graph

---
+++ client/templates/graphics/hardened_concrete/cusum.js
@@ -0,0 +1,54 @@
+var updateChart = function (data) {
+  setTimeout(function () {
+    if ($('[data-chart]').length) {
+      var options = {
+        showLine:   true,
+        lineSmooth: false,
+        axisX: {
+          labelInterpolationFnc: function (value, index) {
+            var module = Math.round(data.labels.length / 24)
+
+            return index % module === 0 ? value : null
+          }
+        },
+        plugins: [
+          Chartist.plugins.tooltip(),
+          Chartist.plugins.ctAxisTitle({
+            axisX: {
+              axisTitle: '',
+              axisClass: 'ct-axis-title',
+              offset: {
+                x: 0,
+                y: 0
+              }
+            },
+            axisY: {
+              axisTitle: 'MPa',
+              axisClass: 'ct-axis-title',
+              offset: {
+                x: 0,
+                y: 10
+              },
+              textAnchor: 'middle',
+              flipTitle: true
+            }
+          })
+        ]
+      }
+
+      new Chartist.Line('[data-chart]', data, options)
+    }
+  }, 200)
+}
+
+Template.graphicHardenedConcreteCusum.onRendered(function () {
+  updateChart(_.pick(this.data, 'labels', 'series'))
+})
+
+Template.graphicHardenedConcreteCusum.helpers({
+  sampleCount: function () {
+    updateChart(_.pick(this, 'labels', 'series'))
+
+    return this.samples && this.samples.count()
+  }
+})
