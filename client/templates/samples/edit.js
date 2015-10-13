fe58d8d02c818e2452c11e82925aa842b4f7aa58
Franco Catena <francocatena@gmail.com>
2015-10-13 20:53:01 -0300

    Add warning about sample cracks re-generation

--- client/templates/samples/edit.js
+++ client/templates/samples/edit.js
@@ -1,5 +1,23 @@
+var dateWarning = new ReactiveVar
+
+Template.sampleEdit.onDestroyed(function () {
+  dateWarning.set()
+})
+
 Template.sampleEdit.helpers({
   receipt: function () {
     return Receipts.findOne({ sampleId: this._id })
+  },
+
+  dateWarning: function () {
+    return dateWarning.get()
+  }
+})
+
+Template.sampleEdit.events({
+  'dp.change [name="date"]': function (event, template) {
+    var warning = ! moment(template.data.sample.date).isSame(event.date, 'day')
+
+    dateWarning.set(warning)
   }
 })
