97ce6020e700699f43b7bba5299f1f1946ebbf83
Franco Catena <francocatena@gmail.com>
2015-11-18 19:44:06 -0300

    Little improvement on granulometries helper

--- client/templates/granulometries/granulometry.js
+++ client/templates/granulometries/granulometry.js
@@ -75,7 +75,7 @@ Template.granulometry.helpers({
     var materialId    = this.materialId
     var materialList  = Materials.first()
     var materials     = materialList && materialList[this.type + 's']
-    var material      = _.find(materials, function (m) { return m._id === materialId })
+    var material      = _.findWhere(materials, { _id: materialId })
 
     return material && material.name
   },
