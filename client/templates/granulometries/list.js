2617e18abaa7d6d1ae5917a0995b618ecca55ddb
Franco Catena <francocatena@gmail.com>
2015-11-18 19:43:42 -0300

    Add material to granulometries list

--- client/templates/granulometries/list.js
+++ client/templates/granulometries/list.js
@@ -1,5 +1,14 @@
 Template.granulometriesList.helpers({
   plant: function (plantId) {
     return plantId && Plants.findOne(plantId).name
-  }
+  },
+
+  material: function () {
+    var materialId    = this.materialId
+    var materialList  = Materials.first()
+    var materials     = materialList && materialList[this.type + 's']
+    var material      = _.findWhere(materials, { _id: materialId })
+
+    return material && material.name
+  },
 })
