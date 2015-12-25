44ad1fd1ed4f44c671b80960e12a9a29d01b158a
Franco Catena <francocatena@gmail.com>
2015-12-25 11:36:07 -0300

    Now you can search on customer's cracks

--- client/templates/customers/cracks.js
+++ client/templates/customers/cracks.js
@@ -22,3 +22,42 @@ Template.customerCracks.helpers({
     return moment.localeData().relativeTime(days, false, days === 1 ? 'd' : 'dd')
   }
 })
+
+Template.customerCracks.events({
+  'click [data-action="search"]': function (event, template) {
+    var molding = template.$('#molding').val()
+    var cracked = template.$('#cracked').val()
+
+    var search = {
+      receipt:  template.$('#receipt').val(),
+      work:     template.$('#work').val(),
+      molding:  molding && moment(molding, 'L').format('YYYY-MM-DD'),
+      cracked:  cracked && moment(cracked, 'L').format('YYYY-MM-DD'),
+      strength: template.$('#strength').val(),
+      age:      template.$('#age').val(),
+      stress:   template.$('#stress').val()
+    }
+
+    Router.go('customerCracks', { _id: template.data.customer._id }, { query: search })
+  },
+
+  'click [data-search-clean]': function (event, template) {
+    template.$('#search-filter').collapse('hide')
+  },
+
+  'shown.bs.collapse': function (event, template) {
+    template.$('input:first').focus()
+
+    template.$('#molding').datetimepicker({ format: 'L' })
+    template.$('#cracked').datetimepicker({ format: 'L' })
+
+    template.$('[data-search-clean]').removeClass('hidden')
+    template.$('[data-search]').addClass('hidden')
+  },
+
+  'hidden.bs.collapse': function (event, template) {
+    template.$('[data-search-clean]').addClass('hidden')
+    template.$('[data-search]').removeClass('hidden')
+    template.$('input').val('')
+  }
+})
