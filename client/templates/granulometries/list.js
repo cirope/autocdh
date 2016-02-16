89194e8f704b5c32c39f85f29525317910c4ec97
Franco Catena <francocatena@gmail.com>
2016-02-15 21:49:20 -0300

    Search granulometries

--- client/templates/granulometries/list.js
+++ client/templates/granulometries/list.js
@@ -1,3 +1,9 @@
+Template.granulometries.helpers({
+  showList: function () {
+    return this.granulometries.count() || this.hasQuery
+  }
+})
+
 Template.granulometriesList.helpers({
   plant: function (plantId) {
     return plantId && Plants.findOne(plantId).name
@@ -10,5 +16,41 @@ Template.granulometriesList.helpers({
     var material      = _.findWhere(materials, { _id: materialId })
 
     return material && material.name
+  }
+})
+
+Template.granulometriesList.events({
+  'click [data-action="search"]': function (event, template) {
+    var dateRange = DateRangeHelper.getRange(template.$('#date'))
+
+    var search = {
+      name:     template.$('#name').val(),
+      material: template.$('#material').val(),
+      date:     dateRange && dateRange.join('|'),
+      plant:    template.$('#plant').val()
+    }
+
+    Router.go('granulometries', {}, { query: search })
   },
+
+  'click [data-search-clean]': function (event, template) {
+    template.$('#search-filter').collapse('hide')
+  },
+
+  'shown.bs.collapse': function (event, template) {
+    template.$('input:first').focus()
+
+    template.$('#date').
+      daterangepicker(DateRangeHelper.filterOptions()).
+      daterangepickerFilterEvents()
+
+    template.$('[data-search-clean]').removeClass('hidden')
+    template.$('[data-search]').addClass('hidden')
+  },
+
+  'hidden.bs.collapse': function (event, template) {
+    template.$('[data-search-clean]').addClass('hidden')
+    template.$('[data-search]').removeClass('hidden')
+    template.$('input').val('')
+  }
 })
