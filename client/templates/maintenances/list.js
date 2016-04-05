f3ff20472a4e09609d8e7d744120684b4b7009c5
Franco Catena <francocatena@gmail.com>
2016-04-05 20:40:00 -0300

    Display warning label alongside maintenances

--- client/templates/maintenances/list.js
+++ client/templates/maintenances/list.js
@@ -1,5 +1,17 @@
 Template.maintenancesList.helpers({
   type: function () {
     return TAPi18n.__('maintenance_' + this.type)
+  },
+
+  displayWarning: function () {
+    var warningDate = moment().add(15, 'days')
+
+    return warningDate.isAfter(this.validUntil)
+  },
+
+  labelClass: function () {
+    var expired = moment().isAfter(this.validUntil)
+
+    return expired ? 'label-danger' : 'label-warning'
   }
 })
