53ab0f9fef15eef9aae91e3cec10740991336943
Franco Catena <francocatena@gmail.com>
2016-05-22 20:28:26 -0300

    Add provider, origin and receipt to granulometries

--- client/templates/granulometries/edit.js
+++ client/templates/granulometries/edit.js
@@ -1,3 +1,9 @@
 Template.granulometryEdit.onCreated(function () {
   Granulometries.setType(this.data.type)
 })
+
+Template.granulometryEdit.helpers({
+  provider: function () {
+    return this.providerId && Providers.findOne(this.providerId).name
+  }
+})
