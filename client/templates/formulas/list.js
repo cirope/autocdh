500b1ebe32851853f0931172cbf10fa5a032bb00
Franco Catena <francocatena@gmail.com>
2016-05-24 19:14:57 -0300

    Search in formulas

--- client/templates/formulas/list.js
+++ client/templates/formulas/list.js
@@ -1,3 +1,9 @@
+Template.formulas.helpers({
+  showList: function () {
+    return this.formulas.count() || this.hasQuery
+  }
+})
+
 Template.formulasList.helpers({
   strength: function (strengthId) {
     return Strengths.findOne(strengthId).name
@@ -13,5 +19,49 @@ Template.formulasList.helpers({
 
   downloadName: function (download) {
     return TAPi18n.__('download_' + download)
+  },
+
+  downloadOptions: function () {
+    var settings = Settings.findOne()
+    var options  = [
+      { value: 'canal', label: TAPi18n.__('download_canal') },
+      { value: 'pump',  label: TAPi18n.__('download_pump') }
+    ]
+
+    if (settings && settings.customOptions && settings.customOptions.showPavement)
+      options.push({ value: 'pavement', label: TAPi18n.__('download_pavement') })
+
+    return options
+  }
+})
+
+Template.formulasList.events({
+  'click [data-action="search"]': function (event, template) {
+    var search = {
+      coding:     template.$('#coding').val(),
+      strength:   template.$('#strength').val(),
+      download:   template.$('#download').val(),
+      aggregate:  template.$('#aggregate').val(),
+      settlement: template.$('#settlement').val()
+    }
+
+    Router.go('formulas', {}, { query: search })
+  },
+
+  'click [data-search-clean]': function (event, template) {
+    template.$('#search-filter').collapse('hide')
+  },
+
+  'shown.bs.collapse': function (event, template) {
+    template.$('input:first').focus()
+
+    template.$('[data-search-clean]').removeClass('hidden')
+    template.$('[data-search]').addClass('hidden')
+  },
+
+  'hidden.bs.collapse': function (event, template) {
+    template.$('[data-search-clean]').addClass('hidden')
+    template.$('[data-search]').removeClass('hidden')
+    template.$('input').val('')
   }
 })
