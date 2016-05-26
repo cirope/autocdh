076ff78df077f13c63d0a4853776c28f2d72f28e
Franco Catena <francocatena@gmail.com>
2016-05-26 11:34:13 -0300

    Allow PDF download from provided cracks list

--- client/templates/provided_cracks/list.js
+++ client/templates/provided_cracks/list.js
@@ -1,9 +1,25 @@
+var providedCrack = new ReactiveVar
+
 Template.providedCracksList.helpers({
+  providedCrack: function () {
+    return providedCrack.get()
+  },
+
   work: function () {
     return Works.findOne(this.workId).name
   },
 
   customer: function () {
     return Customers.findOne(this.customerId).name
+  }
+})
+
+Template.providedCracksList.events({
+  'click [data-download="pdf"]': function () {
+    providedCrack.set(this)
+
+    setTimeout(function () {
+      $('[data-download-pdf]').click()
+    }, 300)
   }
 })
