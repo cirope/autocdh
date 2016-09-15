7cf8dc3f12716c634090f5cd74e0e5244b10ad34
Franco Catena <francocatena@gmail.com>
2016-09-15 12:24:27 -0300

    Bring back fresh concrete settlement graphic

---
+++ client/templates/graphics/fresh_concrete/settlement.js
@@ -0,0 +1,57 @@
+var updateChart = function (data) {
+  setTimeout(function () {
+    if ($('[data-chart]').length) {
+      var options = {
+        low:   0,
+        axisX: {
+          labelInterpolationFnc: function (value, index) {
+            var module = Math.round(data.labels.length / 12)
+
+            return index % module === 0 ? value : null
+          }
+        },
+        axisY: {
+          scaleMinSpace: 10,
+          onlyInteger: true
+        },
+        plugins: [
+          Chartist.plugins.tooltip(),
+          Chartist.plugins.ctAxisTitle({
+            axisX: {
+              axisTitle: '',
+              axisClass: 'ct-axis-title',
+              offset: {
+                x: 0,
+                y: 0
+              }
+            },
+            axisY: {
+              axisTitle: TAPi18n.__('graphic_fresh_concrete_settlement_y_label'),
+              axisClass: 'ct-axis-title',
+              offset: {
+                x: 0,
+                y: 10
+              },
+              textAnchor: 'middle',
+              flipTitle: true
+            }
+          })
+        ]
+      }
+
+      new Chartist.Line('[data-chart]', data, options)
+    }
+  }, 200)
+}
+
+Template.graphicFreshConcreteSettlement.onRendered(function () {
+  updateChart(_.pick(this.data, 'labels', 'series'))
+})
+
+Template.graphicFreshConcreteSettlement.helpers({
+  sampleCount: function () {
+    updateChart(_.pick(this, 'labels', 'series'))
+
+    return this.samples.count()
+  }
+})
