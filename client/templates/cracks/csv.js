149b1cf7d894424174937ce2bdbdaff3e44e40c6
Luis Enrique Funes <le.funes@gmail.com>
2016-10-18 01:33:18 -0300

    Changed stress round digits

--- client/templates/cracks/csv.js
+++ client/templates/cracks/csv.js
@@ -25,14 +25,10 @@ var putCrack = function (row, crack) {
     }
   }
   row[TAPi18n.__('crack_tube_type')]    = label
-  if(bendingTubeType){
-      row[TAPi18n.__('crack_width')]    = crack.diameter + ' mm'
-      row[TAPi18n.__('crack_height')]   = crack.height + ' mm'
-      row[TAPi18n.__('crack_light')]    = crack.light + ' mm'
-  } else {
-      row[TAPi18n.__('crack_diameter')] = crack.diameter + ' mm'
-      row[TAPi18n.__('crack_height')]       = crack.height + ' mm'
-  }
+  row[TAPi18n.__('crack_diameter')]     = crack.diameter && !bendingTubeType ? crack.diameter + ' mm' : null
+  row[TAPi18n.__('crack_height')]       = crack.height ? crack.height + ' mm' : null
+  row[TAPi18n.__('crack_width')]        = crack.diameter && bendingTubeType ? crack.diameter + ' mm' : null
+  row[TAPi18n.__('crack_light')]        = crack.light && bendingTubeType ? crack.light + ' mm' : null
   row[TAPi18n.__('crack_observations')] = crack.observations || ''
 }
 
@@ -43,7 +39,6 @@ var putCrackedCrack = function (row, crack) {
   row[TAPi18n.__('crack_molding_in')]   = moment(crack.moldingIn).format('L')
   row[TAPi18n.__('crack_cracked_in')]   = moment(crack.crackedIn).format('L')
   row[TAPi18n.__('crack_other_assay')]  = crack.otherAssay ? TAPi18n.__('assay_other_assay_' + crack.otherAssay) : TAPi18n.__('no')
-
 
   var bendingTubeType = false
   var label = TAPi18n.__('no')
@@ -65,18 +60,13 @@ var putCrackedCrack = function (row, crack) {
     }
   }
   row[TAPi18n.__('crack_tube_type')]    = label
-  if(bendingTubeType){
-      row[TAPi18n.__('crack_width')]    = crack.diameter + ' mm'
-      row[TAPi18n.__('crack_height')]   = crack.height + ' mm'
-      row[TAPi18n.__('crack_light')]    = crack.light + ' mm'
-      row[TAPi18n.__('crack_load')]     = crack.load.toFixed(0)
-      row[TAPi18n.__('crack_stress_bending')] = crack.stress.toFixed(1) + ' MPa'
-  } else {
-      row[TAPi18n.__('crack_diameter')] = crack.diameter + ' mm'
-      row[TAPi18n.__('crack_height')]   = crack.height + ' mm'
-      row[TAPi18n.__('crack_load')]     = crack.load.toFixed(0)
-      row[TAPi18n.__('crack_stress')]   = crack.stress.toFixed(1) + ' MPa'
-  }
+  row[TAPi18n.__('crack_diameter')]     = crack.diameter && !bendingTubeType ? crack.diameter + ' mm' : null
+  row[TAPi18n.__('crack_height')]       = crack.height ? crack.height + ' mm' : null
+  row[TAPi18n.__('crack_width')]        = crack.diameter && bendingTubeType ? crack.diameter + ' mm' : null
+  row[TAPi18n.__('crack_light')]        = crack.light && bendingTubeType ? crack.light + ' mm' : null
+  row[TAPi18n.__('crack_load')]         = crack.load ? crack.load.toFixed(0) : null
+  row[TAPi18n.__('crack_stress')]       = crack.stress && !bendingTubeType ? crack.stress.toFixed(1) + ' MPa' : null
+  row[TAPi18n.__('crack_stress_bending')] = crack.stress && bendingTubeType ? crack.stress.toFixed(1) + ' MPa' : null
   row[TAPi18n.__('crack_observations')] = crack.observations || ''
 }
 
