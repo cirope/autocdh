a88663c71c1e2dca0f3363a0feab82412ff8826a
Luis Enrique Funes <le.funes@gmail.com>
2016-10-29 10:40:07 -0300

    Adding signature to pdfs

--- client/lib/digitalSignature.js
+++ client/lib/digitalSignature.js
@@ -7,6 +7,13 @@ DigitalSignature = {
             if(settings && settings.digitalSignature && settings.digitalSignature.enabled && settings.digitalSignature[type]) {
                 console.log("--------------------------------------["+type+"] enabled")
 
+                if (yPosition > 270) {
+                    console.log("--------------------------------------["+type+"] new page!!!")
+                    yPosition = 20
+                    pdf.addPage()
+                }
+
+                console.log("--------------------------------------["+type+"] y="+yPosition)
                 pdf.text("PDFs rocks!!!!!!!!!!!!!!!", 20, yPosition += 10)
             }
         }
--- client/templates/provided_cracks/pdf.js
+++ client/templates/provided_cracks/pdf.js
@@ -116,8 +116,10 @@ Template.providedCrack.events({
           fontSize: 7
         })
 
+      yPosition = doc.lastCellPos.y + 10
+
       // adding digital signature
-      yPosition = DigitalSignature.addSignatureToPdf(doc, 'pdfProvidedCracks', doc.lastCellPos.y + 35)
+      yPosition = DigitalSignature.addSignatureToPdf(doc, 'pdfProvidedCracks', yPosition)
 
       doc.putTotalPages('___total_pages___')
       doc.save(fileName + '.pdf')
