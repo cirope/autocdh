685cb8056722313dac35cb16b143049dae96a8d3
Luis Enrique Funes <le.funes@gmail.com>
2016-12-03 20:54:24 -0300

    Field density

--- client/templates/field_densities/field_density.js
+++ client/templates/field_densities/field_density.js
@@ -1,6 +1,12 @@
 
 
 Template.fieldDensity.helpers({
+    sampleResponsible: function () {
+        return this.sampleResponsibleId && Responsible.findOne(this.sampleResponsibleId).name
+    },
+    assayResponsible: function () {
+        return this.assayResponsibleId && Responsible.findOne(this.assayResponsibleId).name
+    },
 })
 
 Template.fieldDensity.events({
