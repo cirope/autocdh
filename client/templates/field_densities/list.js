72b6469a29c148ba244a0caf9b7ce7b5c7f82fd6
Luis Enrique Funes <le.funes@gmail.com>
2016-12-03 12:33:55 -0300

    Field density

---
+++ client/templates/field_densities/list.js
@@ -0,0 +1,11 @@
+Template.fieldDensities.helpers({
+  showList: function () {
+    return this.fieldDensities.count() || this.hasQuery
+  }
+})
+
+Template.fieldDensitiesList.helpers({
+})
+
+Template.fieldDensitiesList.events({
+})
