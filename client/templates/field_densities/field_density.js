5663497dee21d8716c76e3f98bd5b8d3409b3733
Luis Enrique Funes <le.funes@gmail.com>
2016-12-04 09:48:00 -0300

    Field density

--- client/templates/field_densities/field_density.js
+++ client/templates/field_densities/field_density.js
@@ -6,8 +6,15 @@ Template.fieldDensity.helpers({
     },
     assayResponsible: function () {
         return this.assayResponsibleId && Responsible.findOne(this.assayResponsibleId).name
-    },
+    }
 })
 
 Template.fieldDensity.events({
+    'click [data-delete]': function (event, template) {
+        if (confirm(TAPi18n.__('confirm_delete'))){
+            Meteor.call('removeFieldDensity', template.data._id, function (error) {
+                if (!error) Router.go('fieldDensities')
+            })
+        }
+    }
 })
