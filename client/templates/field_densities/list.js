3f83b67edb84731f79422720b49558e4e5311236
Luis Enrique Funes <le.funes@gmail.com>
2016-12-04 12:25:24 -0300

    Field density

--- client/templates/field_densities/list.js
+++ client/templates/field_densities/list.js
@@ -8,4 +8,42 @@ Template.fieldDensitiesList.helpers({
 })
 
 Template.fieldDensitiesList.events({
+  'click [data-action="search"]': function (event, template) {
+    var fieldDateRange = DateRangeHelper.getRange(template.$('#fieldDate'))
+    var labDateRange = DateRangeHelper.getRange(template.$('#labDate'))
+
+    var search = {
+      sampleName: template.$('#sampleName').val(),
+      work:       template.$('#work').val(),
+      fieldDate:  fieldDateRange && fieldDateRange.join('|'),
+      labDate:    labDateRange && labDateRange.join('|')
+    }
+
+    Router.go('fieldDensities', {}, { query: search })
+  },
+
+  'click [data-search-clean]': function (event, template) {
+    template.$('#search-filter').collapse('hide')
+  },
+
+  'shown.bs.collapse': function (event, template) {
+    template.$('input:first').focus()
+
+    template.$('#fieldDate').
+    daterangepicker(DateRangeHelper.filterOptions()).
+    daterangepickerFilterEvents()
+
+    template.$('#labDate').
+    daterangepicker(DateRangeHelper.filterOptions()).
+    daterangepickerFilterEvents()
+
+    template.$('[data-search-clean]').removeClass('hidden')
+    template.$('[data-search]').addClass('hidden')
+  },
+
+  'hidden.bs.collapse': function (event, template) {
+    template.$('[data-search-clean]').addClass('hidden')
+    template.$('[data-search]').removeClass('hidden')
+    template.$('input').val('')
+  }
 })
