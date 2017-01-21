f856504b2b278c460b20d12270ff9df64a0a31a7
Luis Enrique Funes <le.funes@gmail.com>
2017-01-21 20:41:54 -0300

    Templates > Templates (edit)

--- client/templates/compactions/edit.js
+++ client/templates/compactions/edit.js
@@ -1,3 +1,11 @@
 
+var calculateFields = function () {
+
+}
 Template.compactionEdit.events({
+  /*
+  'change [name="massContainer"], change [name="massContainerWet"], change [name="massContainerDry"]': function (event) {
+    calculateFields()
+  }
+  */
 })
