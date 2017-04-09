1dfa2b1c1337711b17ae2f70a4b4d70f5d7ce1de
Luis Enrique Funes <le.funes@gmail.com>
2017-04-09 01:16:25 -0300

    Fresh concrete PDFs

---
+++ client/templates/graphics/fresh_concrete/air_pdf.js
@@ -0,0 +1,32 @@
+
+
+var putFilterData = function (filter, doc, yPosition) {
+  return PdfHelper.addFilterData(filter, doc, yPosition, true, true, ['plantId', 'strengthId', 'download', 'aggregateId', 'settlementId'])
+}
+
+Template.graphicFreshConcreteAir.events({
+  'click [data-download-pdf]': function (event, template) {
+    var yPosition            = 40
+    var name                 = TAPi18n.__('graphic_fresh_concrete_air_title')
+
+    PDF.new({}, function (doc) {
+      doc
+        .setFont('helvetica')
+        .setFontSize(14)
+        .text(name, 20, yPosition)
+        .setFontSize(9)
+
+      doc.setFontSize(9)
+
+      yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
+
+      PdfHelper.addGraphImage(doc, yPosition += 5, function () {
+        // adding digital signature
+        yPosition = DigitalSignature.addSignatureToEachPage(doc, 'pdfFreshConcrete', function () {
+          doc.putTotalPages('___total_pages___')
+          doc.save(name+'.pdf')
+        })
+      })
+    })
+  }
+})
