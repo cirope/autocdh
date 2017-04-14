6b20152449bb1bb9975eac7a1a2a5e07689cceb3
Luis Enrique Funes <le.funes@gmail.com>
2017-04-14 17:36:17 -0300

    Compaction

--- client/templates/compactions/edit.js
+++ client/templates/compactions/edit.js
@@ -1,3 +1,4 @@
+var _realDensity = new ReactiveVar(false)
 
 var checkType = function (type) {
   var layers = -1;
@@ -25,32 +26,136 @@ var checkType = function (type) {
   }
 };
 
-var calculateFields = function () {
-  checkType($('[name="type"]').val());
+var checkSieve = function (sieve) {
+  _realDensity.set(sieve == '4');
+};
 
-  var t2 = +$('[name="retained"]').val()
+var copyValues = function (name, from, to) {
+  var value = getField(name+from);
+  if(value){
+    for(var i = from+1; i <= to; i++){
+      if(!getField(name+i)){
+        setField(name+i, value)
+      } else {
+        copyValues(name, i, to)
+        break
+      }
+    }
+  } else {
+    if(from < to){
+      copyValues(name, from+1, to)
+    }
+  }
+};
+
+var getField = function (name) {
+  return $('[name="' + name + '"]').val()
+};
+
+var setField = function (name, value) {
+  $('[name="'+name+'"]').val(value)
+};
+
+var calculateFieldDensity = function (pos) {
+  var r = 0;
+
+  var v = getField('volume_p'+pos);
+  if(0 < v && getField('mold_mass_p'+pos) && getField('empty_mold_mass_p'+pos)){
+    r = (getField('mold_mass_p'+pos) - getField('empty_mold_mass_p'+pos)) / v;
+    r = r.toFixed(0)
+  }
+  setField('field_density_p'+pos, r)
+};
+
+var calculateFields = function () {
+  checkType(getField('type'));
+  checkSieve(getField('sieve'));
+
+  var t2 = +getField('retained')
   if(t2){
-    var t1 = +$('[name="through"]').val()
+    var t1 = +getField('through')
     if(!t1){
       t1 = 0;
     }
 
     var r = t2*100/(t2+t1);
     r = r.toFixed(0)
-    $('[name="retained_percentage"]').val(r)
+    setField('retained_percentage', r)
   } else {
-    $('[name="retained_percentage"]').val(0)
+    getField('retained_percentage', 0)
   }
 
+  // copy values to next point
+  copyValues('mold_code_p', 1, 5)
+  copyValues('volume_p', 1, 5)
+  copyValues('water_p', 1, 5)
+  copyValues('empty_mold_mass_p', 1, 5)
 
+  // calculate masses
+  calculateFieldDensity(1);
+  calculateFieldDensity(2);
+  calculateFieldDensity(3);
+  calculateFieldDensity(4);
+  calculateFieldDensity(5);
 
 };
+
+Template.compactionEdit.helpers({
+  showRealDensity: function () {
+    return _realDensity.get()
+  }
+})
 
 Template.compactionEdit.events({
   'change [name="type"]': function (event) {
     checkType($(event.currentTarget).val())
   },
+  'change [name="sieve"]': function (event) {
+    checkSieve($(event.currentTarget).val())
+  },
   'change [name="through"], change [name="retained"]': function (event) {
     calculateFields()
-  }
+  },
+  'change [name="mold_code_p1"], change [name="mold_code_p2"], change [name="mold_code_p3"], change [name="mold_code_p4"], change [name="mold_code_p5"]': function (event) {
+    calculateFields()
+  },
+  'change [name="volume_p1"], change [name="volume_p2"], change [name="volume_p3"], change [name="volume_p4"], change [name="volume_p5"]': function (event) {
+    calculateFields()
+  },
+  'change [name="water_p1"], change [name="water_p2"], change [name="water_p3"], change [name="water_p4"], change [name="water_p5"]': function (event) {
+    calculateFields()
+  },
+  'change [name="empty_mold_mass_p1"], change [name="empty_mold_mass_p2"], change [name="empty_mold_mass_p3"], change [name="empty_mold_mass_p4"], change [name="empty_mold_mass_p5"]': function (event) {
+    calculateFields()
+  },
+  'change [name="mold_mass_p1"], change [name="mold_mass_p2"], change [name="mold_mass_p3"], change [name="mold_mass_p4"], change [name="mold_mass_p5"]': function (event) {
+    calculateFields()
+  },
+  'change [name="container_code_p1"], change [name="container_code_p2"], change [name="container_code_p3"], change [name="container_code_p4"], change [name="container_code_p5"]': function (event) {
+    calculateFields()
+  },
+  'change [name="empty_container_mass_p1"], change [name="empty_container_mass_p2"], change [name="empty_container_mass_p3"], change [name="empty_container_mass_p4"], change [name="empty_container_mass_p5"], ': function (event) {
+    calculateFields()
+  },
+  'change [name="wet_container_mass_p1"], change [name="wet_container_mass_p2"], change [name="wet_container_mass_p3"], change [name="wet_container_mass_p4"], change [name="wet_container_mass_p5"]': function (event) {
+    calculateFields()
+  },
+  'change [name="dry_container_mass_p1"], change [name="dry_container_mass_p2"], change [name="dry_container_mass_p3"], change [name="dry_container_mass_p4"], change [name="dry_container_mass_p5"]': function (event) {
+    calculateFields()
+  }/*,
+  'change [name=""], ': function (event) {
+    calculateFields()
+  },
+  'change [name=""], ': function (event) {
+    calculateFields()
+  },
+  'change [name=""], ': function (event) {
+    calculateFields()
+  },
+  'change [name=""], ': function (event) {
+    calculateFields()
+  },
+  'change [name=""], ': function (event) {
+    calculateFields()
+  }*/
 })
