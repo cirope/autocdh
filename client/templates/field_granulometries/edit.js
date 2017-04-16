9268a449a3fc19a2709b255b96af7bf840dfd437
Luis Enrique Funes <le.funes@gmail.com>
2017-04-16 17:02:39 -0300

    Field gralunometries

--- client/templates/field_granulometries/edit.js
+++ client/templates/field_granulometries/edit.js
@@ -4,11 +4,38 @@ var getField = function (name) {
 	return $('[name="' + name + '"]').val()
 };
 
+var getFloat = function (name) {
+	return parseFloat(getField(name) || 0)
+};
+
 var setField = function (name, value) {
 	$('[name="'+name+'"]').val(value)
 };
 
 var calculateFields = function () {
+	var hp = 0;
+	var he = getFloat('humidity_empty_mass');
+	var hw = getFloat('humidity_wet');
+	var hd = getFloat('humidity_dry');
+	if(hw > hd && hd > he){
+		hp = (hw-hd)/(hd-he)*100
+	}
+	setField('humidity_percentage', hp ? hp.toFixed(1) : '')
+
+	var tp = 0;
+	var te = getFloat('thin_empty_mass');
+	var tb = getFloat('thin_before');
+	var ta = getFloat('thin_after');
+	if(tb > ta && tb > te){
+		tp = (tb-ta)/(tb-te)*100
+	}
+	setField('thin_percentage', tp ? tp.toFixed(1) : '')
+
+	/*
+	var he = getFloat('sieve_weight_76');
+	var hw = getFloat('retained_sieve_76');
+	var hd = getFloat('retained_partial_76');
+	*/
 };
 
 Template.fieldGranulometryEdit.onRendered(function () {
@@ -19,7 +46,10 @@ Template.fieldGranulometryEdit.helpers({
 
 Template.fieldGranulometryEdit.events({
 
-	/*'change [name="liquid_empty_mass_p1"], change [name="liquid_empty_mass_p2"], change [name="liquid_empty_mass_p3"]': function (event) {
+	'change [name="humidity_empty_mass"], change [name="humidity_wet"], change [name="humidity_dry"]': function (event) {
 		calculateFields()
-	},*/
+	},
+	'change [name="thin_empty_mass"], change [name="thin_before"], change [name="thin_after"]': function (event) {
+		calculateFields()
+	},
 })
