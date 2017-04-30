7c7f96097bc87c9e85fdf477525f59cfb425a82a
Franco Catena <francocatena@gmail.com>
2017-04-29 23:02:03 -0300

    Back to show menu only when user

--- client/templates/includes/menu.js
+++ client/templates/includes/menu.js
@@ -1,9 +1,3 @@
-Template.menu.helpers({
-  showNav: function () {
-    return Meteor.user() && ! Session.get('loading')
-  }
-})
-
 Template.menu.events({
   'click a:not(.dropdown-toggle)': function () {
     $('.navbar-fixed-top .navbar-collapse.in').collapse('hide')
