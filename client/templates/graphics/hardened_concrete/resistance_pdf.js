1a995f6aec43efd2b49c2660d5b5e69ce59f08a4
Luis Enrique Funes <le.funes@gmail.com>
2017-06-20 22:39:16 -0300

    Hardened concrete: Resistance

--- client/templates/graphics/hardened_concrete/resistance_pdf.js
+++ client/templates/graphics/hardened_concrete/resistance_pdf.js
@@ -30,5 +30,40 @@ Template.graphicHardenedConcreteResistance.events({
         })
       })
     })
+  },
+  'click [values-download-pdf]': function (event, template) {
+    var yPosition            = 40
+    var name                 = TAPi18n.__('graphic_hardened_concrete_resistance_title')
+
+    PDF.new({}, function (doc) {
+      doc
+        .setFont('helvetica')
+        .setFontSize(14)
+        .text(name, 20, yPosition)
+        .setFontSize(9)
+
+      doc.setFontSize(9)
+
+      yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
+      yPosition += 10
+
+      var table = PdfHelper.miniTable('hardened-concrete-cracks', {
+        widths: [25, 40, 40, 40]
+      })
+      doc
+        .setFontSize(6)
+        .table(PdfHelper.COL_1, yPosition, table.data, table.headers, {
+          printHeaders: true,
+          autoSize: false,
+          margins: { right: 0, left: 0, top: 10, bottom: 0 },
+          fontSize: 7
+        })
+
+      // adding digital signature
+      yPosition = DigitalSignature.addSignatureToEachPage(doc, 'pdfHardenedConcrete', function () {
+        doc.putTotalPages('___total_pages___')
+        doc.save(name+'.pdf')
+      })
+    })
   }
 })
