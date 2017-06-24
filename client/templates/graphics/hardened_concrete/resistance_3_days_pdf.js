83bfa27c25f758e678a55a0ef69357577a8418cc
Luis Enrique Funes <le.funes@gmail.com>
2017-06-24 03:46:41 -0300

    Hardened concrete

--- client/templates/graphics/hardened_concrete/resistance_3_days_pdf.js
+++ client/templates/graphics/hardened_concrete/resistance_3_days_pdf.js
@@ -1,5 +1,4 @@
 
-
 var putFilterData = function (filter, doc, yPosition) {
   return PdfHelper.addFilterData(filter, doc, yPosition, true, true, ['concretes', 'plantId', 'strengthId', 'download', 'aggregateId', 'settlementId',
     {label: 'customerId', text: 'customer-search'}, {label: 'workId', text: 'work-search'}
@@ -8,28 +7,7 @@ var putFilterData = function (filter, doc, yPosition) {
 
 Template.graphicHardenedConcrete3DaysResistance.events({
   'click [graph-download-pdf]': function (event, template) {
-    var yPosition            = 40
-    var name                 = TAPi18n.__('graphic_hardened_concrete_3_days_resistance_title')
-
-    PDF.new({}, function (doc) {
-      doc
-        .setFont('helvetica')
-        .setFontSize(14)
-        .text(name, 20, yPosition)
-        .setFontSize(9)
-
-      doc.setFontSize(9)
-
-      yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
-
-      PdfHelper.addGraphImage(doc, yPosition += 5, 'data-container', 6.75, function () {
-        // adding digital signature
-        yPosition = DigitalSignature.addSignatureToEachPage(doc, 'pdfHardenedConcrete', function () {
-          doc.putTotalPages('___total_pages___')
-          doc.save(name+'.pdf')
-        })
-      })
-    })
+    PdfHelper.generateGraphPage(template, 'data-container', 'graphic_hardened_concrete_3_days_resistance_title', 'pdfHardenedConcrete', putFilterData, 20, 2, 63)
   },
   'click [values-download-pdf]': function (event, template) {
     var yPosition            = 40
--- client/templates/graphics/hardened_concrete/resistance_7_days_pdf.js
+++ client/templates/graphics/hardened_concrete/resistance_7_days_pdf.js
@@ -8,28 +8,7 @@ var putFilterData = function (filter, doc, yPosition) {
 
 Template.graphicHardenedConcrete7DaysResistance.events({
   'click [graph-download-pdf]': function (event, template) {
-    var yPosition            = 40
-    var name                 = TAPi18n.__('graphic_hardened_concrete_7_days_resistance_title')
-
-    PDF.new({}, function (doc) {
-      doc
-        .setFont('helvetica')
-        .setFontSize(14)
-        .text(name, 20, yPosition)
-        .setFontSize(9)
-
-      doc.setFontSize(9)
-
-      yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
-
-      PdfHelper.addGraphImage(doc, yPosition += 5, 'data-container', 6.75, function () {
-        // adding digital signature
-        yPosition = DigitalSignature.addSignatureToEachPage(doc, 'pdfHardenedConcrete', function () {
-          doc.putTotalPages('___total_pages___')
-          doc.save(name+'.pdf')
-        })
-      })
-    })
+    PdfHelper.generateGraphPage(template, 'data-container', 'graphic_hardened_concrete_7_days_resistance_title', 'pdfHardenedConcrete', putFilterData, 20, 2, 63)
   },
   'click [values-download-pdf]': function (event, template) {
     var yPosition            = 40
