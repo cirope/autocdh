dbaee004d76f539a213150d09e43c4a03fdbe4ff
Luis Enrique Funes <le.funes@gmail.com>
2017-06-24 01:46:46 -0300

    Fresh concrete: consistency

--- client/templates/graphics/fresh_concrete/consistency_pdf.js
+++ client/templates/graphics/fresh_concrete/consistency_pdf.js
@@ -6,27 +6,27 @@ var putFilterData = function (filter, doc, yPosition) {
 
 Template.graphicFreshConcreteConsistency.events({
   'click [data-download-pdf]': function (event, template) {
-    var yPosition            = 40
+    var yPosition            = 35
     var name                 = TAPi18n.__('graphic_fresh_concrete_consistency')
 
     PDF.new({}, function (doc) {
-      doc
-        .setFont('helvetica')
-        .setFontSize(14)
-        .text(name, 20, yPosition)
-        .setFontSize(9)
+      PdfHelper.addGraphImage(doc, 64, 'data-container', 4.5, function () {
+        doc
+          .setFont('helvetica')
+          .setFontSize(14)
+          .text(name, 20, yPosition)
+          .setFontSize(9)
 
-      doc.setFontSize(9)
+        doc.setFontSize(9)
 
-      yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
+        yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
 
-      PdfHelper.addGraphImage(doc, yPosition += 5, 'data-container', 6.75, function () {
         // adding digital signature
         yPosition = DigitalSignature.addSignatureToEachPage(doc, 'pdfFreshConcrete', function () {
           doc.putTotalPages('___total_pages___')
           doc.save(name+'.pdf')
         })
-      })
+      }, -4)
     })
   }
 })
