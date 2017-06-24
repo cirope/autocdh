adc75af11149a6b23e939cbb409df7017b6e372b
Luis Enrique Funes <le.funes@gmail.com>
2017-06-24 04:16:13 -0300

    Stats aggregates

--- client/templates/graphics/aggregates/fineness_pdf.js
+++ client/templates/graphics/aggregates/fineness_pdf.js
@@ -1,32 +1,10 @@
 
-
 var putFilterData = function (filter, doc, yPosition) {
   return PdfHelper.addFilterData(filter, doc, yPosition, true, true, ['plantId', 'materialId', {label: 'providerId', text: 'provider-search'}])
 }
 
 Template.graphicAggregateFineness.events({
   'click [download-pdf]': function (event, template) {
-    var yPosition            = 40
-    var name                 = TAPi18n.__('graphic_hardened_concrete_cusum_title')
-
-    PDF.new({}, function (doc) {
-      doc
-        .setFont('helvetica')
-        .setFontSize(14)
-        .text(name, 20, yPosition)
-        .setFontSize(9)
-
-      doc.setFontSize(9)
-
-      yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
-
-      PdfHelper.addGraphImage(doc, yPosition += 5, 'data-container', 6.75, function () {
-        // adding digital signature
-        yPosition = DigitalSignature.addSignatureToEachPage(doc, 'pdfStatsAggregates', function () {
-          doc.putTotalPages('___total_pages___')
-          doc.save(name+'.pdf')
-        })
-      })
-    })
+    PdfHelper.generateGraphPage(template, 'data-container', 'graphic_aggregate_fineness_title', 'pdfStatsAggregates', putFilterData)
   }
 })
