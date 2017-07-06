0430c6103c8d8161a34a4fe8c67487fbf8ee1e51
Luis Enrique Funes <le.funes@gmail.com>
2017-07-06 20:42:01 -0300

    Field granulometry: fixes

--- client/templates/field_granulometries/edit.js
+++ client/templates/field_granulometries/edit.js
@@ -80,8 +80,12 @@ var calculateFields = function () {
 		setField('thin_ver_ret_label', '-')
 	}
 
-	var tr = getField('thin_reduce')
 	var tw = getAndSetFloat('thin_weight')
+	var tr = getField('thin_reduce')
+	if((!tr || tr !== 'yes') && !tw && !!ss){
+		tw = ss
+		setFloat('thin_weight', tw)
+	}
 
 	var sw20 = getAndSetFloat('sieve_weight_20');
 	var sw04 = getAndSetFloat('sieve_weight_04');
@@ -254,145 +258,6 @@ var calculateFields = function () {
 	var total = a3+b3+c3+d3+e3+f3+g3+h3+i3+j3+k3+l3+m3+m5
 	setFloat('total', total)
 
-/*
-
-	var p76 = getFloat('retained_partial_76');
-	var t76 = p76;
-
-	setField('retained_total_76', t76 ? t76.toFixed(0) : '');
-	var p63 = getFloat('retained_partial_63');
-	var t63 = p63 + t76;
-
-	setField('retained_total_63', t63 ? t63.toFixed(0) : '');
-	var p51 = getFloat('retained_partial_51');
-	var t51 = p51 + t63;
-
-	setField('retained_total_51', t51 ? t51.toFixed(0) : '');
-	var p38 = getFloat('retained_partial_38');
-	var t38 = p38 + t51;
-
-	setField('retained_total_38', t38 ? t38.toFixed(0) : '');
-	var p25 = getFloat('retained_partial_25');
-	var t25 = p25 + t38;
-
-	setField('retained_total_25', t25 ? t25.toFixed(0) : '');
-	var p19 = getFloat('retained_partial_19');
-
-	var t19 = p19 + t25;
-	setField('retained_total_19', t19 ? t19.toFixed(0) : '');
-	var p95 = getFloat('retained_partial_95');
-
-	var t95 = p95 + t19;
-	setField('retained_total_95', t95 ? t95.toFixed(0) : '');
-	var p48 = s48 > w48 ? s48 - w48 : 0;
-	setField('retained_partial_48', p48 ? p48.toFixed(0) : '');
-
-	var t48 = p48 + t95;
-	setField('retained_total_48', t48 ? t48.toFixed(0) : '');
-	var p20 = s20 > w20 ? s20 - w20 : 0;
-	setField('retained_partial_20', p20 ? p20.toFixed(0) : '');
-
-	var t20 = p20 + t48;
-	setField('retained_total_20', t20 ? t20.toFixed(0) : '');
-	var p04 = s04 > w04 ? s04 - w04 : 0;
-	setField('retained_partial_04', p04 ? p04.toFixed(0) : '');
-
-	var t04 = p04 + t20;
-	setField('retained_total_04', t04 ? t04.toFixed(0) : '');
-	var p02 = s02 > w02 ? s02 - w02 : 0;
-	setField('retained_partial_02', p02 ? p02.toFixed(0) : '');
-
-	var t02 = p02 + t04;
-	setField('retained_total_02', t02 ? t02.toFixed(0) : '');
-	var p01 = s01 > w01 ? s01 - w01 : 0;
-	setField('retained_partial_01', p01 ? p01.toFixed(0) : '');
-
-	var t01 = p01 + t02;
-	setField('retained_total_01', t01 ? t01.toFixed(0) : '');
-	var p00 = s00 > w00 ? s00 - w00 : 0;
-	setField('retained_partial_00', p00 ? p00.toFixed(0) : '');
-
-	var t00 = p00 + t01;
-	setField('retained_total_00', t00 ? t00.toFixed(0) : '');
-	var pbb = sbb > wbb ? sbb - wbb : 0;
-	setField('retained_partial_b', pbb ? pbb.toFixed(0) : '');
-	var tbb = pbb + t00;
-	setField('retained_total_b', tbb ? tbb.toFixed(0) : '');
-
-	var total = p76 + p63 + p51 + p38 + p25 + p19 + p95 + p48 + p20 + p04 + p02 + p01 + p00 + pbb
-	setField('total', total ? total.toFixed(0) : '');
-
-	var ps76 = total - t76
-	setField('passed_76', ps76 ? ps76.toFixed(0) : '');
-	var po76 = ps76/total*100
-	setField('passed_percentage_76', po76 ? po76.toFixed(0) : '');
-
-	var ps63 = total - t63
-	setField('passed_63', ps63 ? ps63.toFixed(0) : '');
-	var po63 = ps63/total*100
-	setField('passed_percentage_63', po63 ? po63.toFixed(0) : '');
-
-	var ps51 = total - t51
-	setField('passed_51', ps51 ? ps51.toFixed(0) : '');
-	var po51 = ps51/total*100
-	setField('passed_percentage_51', po51 ? po51.toFixed(0) : '');
-
-	var ps38 = total - t38
-	setField('passed_38', ps38 ? ps38.toFixed(0) : '');
-	var po38 = ps38/total*100
-	setField('passed_percentage_38', po38 ? po38.toFixed(0) : '');
-
-	var ps25 = total - t25
-	setField('passed_25', ps25 ? ps25.toFixed(0) : '');
-	var po25 = ps25/total*100
-	setField('passed_percentage_25', po25 ? po25.toFixed(0) : '');
-
-	var ps19 = total - t19
-	setField('passed_19', ps19 ? ps19.toFixed(0) : '');
-	var po19 = ps19/total*100
-	setField('passed_percentage_19', po19 ? po19.toFixed(0) : '');
-
-	var ps95 = total - t95
-	setField('passed_95', ps95 ? ps95.toFixed(0) : '');
-	var po95 = ps95/total*100
-	setField('passed_percentage_95', po95 ? po95.toFixed(0) : '');
-
-	var ps48 = total - t48
-	setField('passed_48', ps48 ? ps48.toFixed(0) : '');
-	var po48 = ps48/total*100
-	setField('passed_percentage_48', po48 ? po48.toFixed(0) : '');
-
-	var ps20 = total - t20
-	setField('passed_20', ps20 ? ps20.toFixed(0) : '');
-	var po20 = ps20/total*100
-	setField('passed_percentage_20', po20 ? po20.toFixed(0) : '');
-
-	var ps04 = total - t04
-	setField('passed_04', ps04 ? ps04.toFixed(0) : '');
-	var po04 = ps04/total*100
-	setField('passed_percentage_04', po04 ? po04.toFixed(0) : '');
-
-	var ps02 = total - t02
-	setField('passed_02', ps02 ? ps02.toFixed(0) : '');
-	var po02 = ps02/total*100
-	setField('passed_percentage_02', po02 ? po02.toFixed(0) : '');
-
-	var ps01 = total - t01
-	setField('passed_01', ps01 ? ps01.toFixed(0) : '');
-	var po01 = ps01/total*100
-	setField('passed_percentage_01', po01 ? po01.toFixed(0) : '');
-
-	var ps00 = total - t00
-	setField('passed_00', ps00 ? ps00.toFixed(0) : '');
-	var po00 = ps00/total*100
-	setField('passed_percentage_00', po00 ? po00.toFixed(0) : '');
-
-	var psbb = total - tbb
-	setField('passed_b', psbb ? psbb.toFixed(0) : '');
-	var pobb = psbb/total*100
-	setField('passed_percentage_b', pobb ? pobb.toFixed(0) : '');
-
-*/
 };
 
 Template.fieldGranulometryEdit.onRendered(function () {
@@ -420,8 +285,24 @@ Template.fieldGranulometryEdit.events({
 	'change [name="retained_sieve_76"], change [name="retained_sieve_63"], change [name="retained_sieve_51"], change [name="retained_sieve_38"], change [name="retained_sieve_25"], change [name="retained_sieve_19"], change [name="retained_sieve_12"], change [name="retained_sieve_95"], change [name="retained_sieve_48"]': function (event) {
 		calculateFields()
 	},
+	'change [name="thin_reduce"] ': function (event) {
 
-	'change [name="thin_reduce"], change [name="thin_weight"] ': function (event) {
+		var tr = getField('thin_reduce')
+		if(!tr || tr !== 'yes'){
+			var tw = getAndSetFloat('thin_weight')
+			if(!tw) {
+				var ss = getAndSetFloat('separation_sieve')
+				if(!!ss) {
+					setFloat('thin_weight', ss)
+				}
+			}
+		} else {
+			setFloat('thin_weight', 0)
+		}
+
+		calculateFields()
+	},
+	'change [name="thin_weight"] ': function (event) {
 		calculateFields()
 	},
 	'change [name="sieve_weight_20"], change [name="sieve_weight_04"], change [name="sieve_weight_01"], change [name="sieve_weight_p"]': function (event) {
@@ -430,11 +311,6 @@ Template.fieldGranulometryEdit.events({
 	'change [name="retained_sieve_20"], change [name="retained_sieve_04"], change [name="retained_sieve_01"], change [name="retained_sieve_p"]': function (event) {
 		calculateFields()
 	},
-	/*
-	'change [name="thin_ver_ret"], change [name="thin_ver_pass"]': function (event) {
-		calculateFields()
-	},
-	*/
 	'change [name="thin_pass_200"]': function (event) {
 		calculateFields()
 	}
