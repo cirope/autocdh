9c962361fd6504e21cbbd9b8c1a72c4c72c55309
Luis Enrique Funes <le.funes@gmail.com>
2017-07-16 19:13:43 -0300

    Resistence by category

--- client/templates/stats/graphics/hardened_concrete/resistance_category.js
+++ client/templates/stats/graphics/hardened_concrete/resistance_category.js
@@ -18,6 +18,44 @@ Template.graphicHardenedConcreteResistanceByCategory.helpers({
 Template.graphicHardenedConcreteResistanceByCategory.events({
   'click [data-download-pdf]': function (event, template) {
 
+    PDF.new({}, function (doc) {
+      var title = TAPi18n.__('stats_resistance_by_category')
+      var yPosition = 30
+
+      doc
+        .setFont('helvetica')
+        .setFontSize(14)
+        .text(title, 20, yPosition)
+
+      yPosition = putFilterData(template.data.filter, doc, yPosition += 5)
+      yPosition += 10
+
+      var table = PdfHelper.miniTable('hardened-concrete-cracks', {
+        widths: [20, 20, 50, 50, 35, 35, 52, 20, 35, 5],
+        sessionHiddenClass: 'text-muted'
+      })
+
+      table.headers.pop()
+      table.headers.splice(2, 2)
+
+      doc
+        .setFontSize(6)
+        .table(PdfHelper.COL_1, yPosition, table.data, table.headers, {
+          printHeaders: true,
+          autoSize: false,
+          margins: { right: 0, left: 0, top: 35, bottom: DigitalSignature.getSignatureHeight(doc, 'pdfHardenedConcrete') },
+          fontSize: 7
+        })
+
+      // adding digital signature
+      yPosition = DigitalSignature.addCenteredSignatureToEachPage(doc, 'pdfHardenedConcrete', function () {
+        doc.putTotalPages('___total_pages___')
+        doc.save(title+'.pdf')
+      })
+    })
+  },
+  'click [data-download-complete-pdf]': function (event, template) {
+
     PDF.new({ orientation: 'l' }, function (doc) {
       var title = TAPi18n.__('stats_resistance_by_category')
       var yPosition = 30
@@ -31,7 +69,7 @@ Template.graphicHardenedConcreteResistanceByCategory.events({
       yPosition += 10
 
       var table = PdfHelper.miniTable('hardened-concrete-cracks', {
-        widths: [20, 20, 70, 70, 35, 35, 50, 20, 35],
+        widths: [20, 20, 70, 70, 35, 35, 52, 20, 35, 5],
         sessionHiddenClass: 'text-muted'
       })
 
