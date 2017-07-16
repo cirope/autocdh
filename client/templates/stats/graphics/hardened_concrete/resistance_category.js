a89915fd5135552e0a71d1338eac59733c993997
Luis Enrique Funes <le.funes@gmail.com>
2017-07-15 22:49:09 -0300

    Resistence by category

--- client/templates/stats/graphics/hardened_concrete/resistance_category.js
+++ client/templates/stats/graphics/hardened_concrete/resistance_category.js
@@ -8,3 +8,36 @@ Template.graphicHardenedConcreteResistanceByCategory.helpers({
     return this.cracks ? this.cracks.length : '-'
   }
 })
+
+Template.graphicHardenedConcreteResistanceByCategory.events({
+  'click [data-download-pdf]': function (event, template) {
+    var yPosition = 20
+    var tableData = table()
+
+    PDF.new({ orientation: 'l' }, function (doc) {
+      var title = TAPi18n.__('customer_cracks_title', {
+        customer: template.data.customer.name
+      })
+
+      doc
+        .setFont('helvetica')
+        .setFontSize(14)
+        .text(title, 20, yPosition)
+
+      doc
+        .setFontSize(9)
+        .table(20, yPosition += 15, tableData.data, tableData.headers, {
+          printHeaders: true,
+          autoSize: false,
+          margins: { right: 0, left: 0, top: 35, bottom: DigitalSignature.getSignatureHeight(doc, 'pdfCustomerCracks') },
+          fontSize: 9
+        })
+
+      // adding digital signature
+      yPosition = DigitalSignature.addCenteredSignatureToEachPage(doc, 'pdfCustomerCracks', function () {
+        doc.putTotalPages('___total_pages___')
+        doc.save(title+'.pdf')
+      })
+    })
+  },
+})
