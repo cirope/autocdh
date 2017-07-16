64aa62d8d8c2d9428c3b94a3eb2d96facee87fa9
Luis Enrique Funes <le.funes@gmail.com>
2017-07-15 23:01:01 -0300

    Resistence by category

--- client/templates/stats/graphics/hardened_concrete/resistance_category.js
+++ client/templates/stats/graphics/hardened_concrete/resistance_category.js
@@ -12,7 +12,6 @@ Template.graphicHardenedConcreteResistanceByCategory.helpers({
 Template.graphicHardenedConcreteResistanceByCategory.events({
   'click [data-download-pdf]': function (event, template) {
     var yPosition = 20
-    var tableData = table()
 
     PDF.new({ orientation: 'l' }, function (doc) {
       var title = TAPi18n.__('customer_cracks_title', {
@@ -24,17 +23,21 @@ Template.graphicHardenedConcreteResistanceByCategory.events({
         .setFontSize(14)
         .text(title, 20, yPosition)
 
+      var table = PdfHelper.miniTable('hardened-concrete-cracks', {
+        widths: [25, 40, 40, 40, 20, 20, 20, 20, 20]
+      })
+
       doc
-        .setFontSize(9)
-        .table(20, yPosition += 15, tableData.data, tableData.headers, {
+        .setFontSize(6)
+        .table(PdfHelper.COL_1, yPosition, table.data, table.headers, {
           printHeaders: true,
           autoSize: false,
-          margins: { right: 0, left: 0, top: 35, bottom: DigitalSignature.getSignatureHeight(doc, 'pdfCustomerCracks') },
-          fontSize: 9
+          margins: { right: 0, left: 0, top: 10, bottom: DigitalSignature.getSignatureHeight(doc, 'pdfHardenedConcrete') },
+          fontSize: 7
         })
 
       // adding digital signature
-      yPosition = DigitalSignature.addCenteredSignatureToEachPage(doc, 'pdfCustomerCracks', function () {
+      yPosition = DigitalSignature.addCenteredSignatureToEachPage(doc, 'pdfHardenedConcrete', function () {
         doc.putTotalPages('___total_pages___')
         doc.save(title+'.pdf')
       })
